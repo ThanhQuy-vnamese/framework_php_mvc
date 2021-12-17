9ae87acc57b9f0c4a2423ed142d234623e8afe91
Dang Trung Hieu <hieu-dang@cybozu.vn>
2021-12-17 22:29:06 +0700

    `PHPMVC` - Fix error syntax when run webpack with Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const path = require('path');
+// eslint-disable-next-line no-undef
+require('@babel/polyfill');
 
+// eslint-disable-next-line no-undef
 module.exports = {
-    entry: path.join(__dirname, "public/js/src", "index.js"),
+    entry: [
+        '@babel/polyfill',
+        // eslint-disable-next-line no-undef
+        path.join(__dirname, 'public/js/src', 'index.js')
+    ],
     output: {
-        path:path.resolve(__dirname, "public/js/dist"),
-        filename: "index.js"
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, 'public/js/dist'),
+        filename: 'index.js'
     },
-    devtool: "inline-source-map",
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.(js|jsx|tsx)$/,
+                test: /\.(js|ts|jsx|tsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
                     }
                 }
-            },
+            }
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx', '.tsx'],
+        extensions: ['*', '.js', '.ts', '.jsx', '.tsx']
     }
-}
+};
