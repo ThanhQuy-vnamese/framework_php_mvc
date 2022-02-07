3bccdadb1541068c87200a7978c99fa54b37488d
trunghieu0207 <dangtrunghieu0207@gmail.com>
2022-02-07 15:40:50 +0700

    `Admin` - Complete manage medical file

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Controllers\MedicalFileController;
 use App\Controllers\UserController;
 use App\Controllers\ApiController;
 use App\Controllers\SampleController;
@@ -44,8 +45,11 @@ $app->router->get('/', [SampleController::class, 'index']);
 $app->router->get('/api/users', [ApiController::class, 'getUser']);
 $app->router->post('/api/login', [ApiController::class, 'login']);
 
+// Admin
 $app->router->get('/admin', [UserController::class, 'getViewUserList']);
 $app->router->get('/admin/user-detail', [UserController::class, 'getViewUserDetail']);
 $app->router->get('/admin/user-add', [UserController::class, 'getViewUserAdd']);
+$app->router->get('/admin/medical-file-list', [MedicalFileController::class, 'getViewMedicalFileList']);
+$app->router->get('/admin/medical-file-detail', [MedicalFileController::class, 'getViewMedicalFileDetail']);
 
 $app->run();
