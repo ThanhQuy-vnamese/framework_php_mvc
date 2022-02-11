e51ad414cade718b9f0815e9f81d6da0fb87f229
trunghieu0207 <dangtrunghieu0207@gmail.com>
2022-02-11 08:39:40 +0700

    `CI` - Edit deploy.sh and add .env server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-pwd
+cd /var/www/phpmvc
+git fetch
+git reset --hard origin/setup-ci
+if [ ! -d "/var/www/phpmvc/vendor" ]; then
+    composer install
+else
+    rm -rf vendor
+    composer install
+fi
+
+if [ ! -d "/var/www/phpmvc/node_modules" ]; then
+    npm install
+else
+    rm -rf node_modules
+    npm install
+fi
+
+npm run build
+chown -R apache.apache /var/www/phpmvc
+chmod -R 755 /var/www/phpmvc
+
+if [  -f "/var/www/phpmvc/.env" ]; then
+    rm -rf /var/www/phpmvc/.env
+    cp -rf /home/.env.server .env
+else
+    cp -rf /home/.env.server .env
+fi
+
+service httpd restart
