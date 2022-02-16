f067a9a6c2dea75398d3720c23ac3158c4ac80f1
trunghieu0207 <dangtrunghieu0207@gmail.com>
2022-02-16 07:13:47 +0700

    `CI` - Reformat scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 cd /var/www/ || exit 1
 if [ ! -d "/var/www/phpmvc" ]; then
-    git clone https://huntercq8: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@github.com/trunghieu0207/phpmvc.git
+  git clone https://huntercq8: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@github.com/trunghieu0207/phpmvc.git
+  chown -R apache.apache /var/www/phpmvc
+  chmod -R 755 /var/www/phpmvc
 fi
 
 cd phpmvc/ || exit 1
 git fetch
 git reset --hard origin/setup-ci
 if [ ! -d "/var/www/phpmvc/vendor" ]; then
-    composer install
+  composer install
 else
-    rm -rf vendor
-    composer install
+  rm -rf vendor
+  composer install
 fi
 
 if [ ! -d "/var/www/phpmvc/node_modules" ]; then
-    npm install
+  npm install
 else
-    rm -rf node_modules
-    npm install
+  rm -rf node_modules
+  npm install
 fi
 
 npm run build
-chown -R apache.apache /var/www/phpmvc
-chmod -R 755 /var/www/phpmvc
 
-if [  -f "/var/www/phpmvc/.env" ]; then
-    rm -rf /var/www/phpmvc/.env
-    cp -rf /var/www/phpmvc/.env.server .env || exit 1
+if [ -f "/var/www/phpmvc/.env" ]; then
+  rm -rf /var/www/phpmvc/.env
+  cp -rf /var/www/phpmvc/.env.server .env || exit 1
 else
-    cp -rf /var/www/phpmvc/.env.server .env || exit 1
+  cp -rf /var/www/phpmvc/.env.server .env || exit 1
 fi
 
 service httpd restart
