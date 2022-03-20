076fe901b67d871081e99ff86cb2b1c5c5ef2c40
trunghieu0207 <dangtrunghieu0207@gmail.com>
2022-03-20 17:43:47 +0700

    `Admin` - Refactor add health

--- app/domain/repository/HealthRepository.php
+++ app/domain/repository/HealthRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\domain\repository;
 
 use App\Core\Database\Query;
+use App\domain\entity\Health;
 
 class HealthRepository implements HealthRepositoryInterface
 {
@@ -29,5 +30,14 @@ class HealthRepository implements HealthRepositoryInterface
         }
 
         return false;
+    }
+
+    public function addHealth(Health $health): int
+    {
+        $sql = "INSERT INTO medical_healths (summary, healths, note, id_medical_records) VALUES ('%s', '%s', '%s', %s);";
+        $query = sprintf($sql, $health->getSummary(), $health->getHealth(), $health->getNote(), $health->getMedicalFileId());
+        $this->db->query($query);
+
+        return (int)$this->db->insert_id;
     }
 }
--- app/domain/repository/HealthRepositoryInterface.php
+++ app/domain/repository/HealthRepositoryInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\domain\repository;
 
+use App\domain\entity\Health;
+
 interface HealthRepositoryInterface
 {
     public function deleteHealth(int $id): bool;
+
+    public function addHealth(Health $health): int;
 }
