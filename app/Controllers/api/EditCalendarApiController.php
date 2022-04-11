342c671a3d7a477bfdfd1fa8c45613ca8bd1cdfa
trunghieu0207 <dangtrunghieu0207@gmail.com>
2022-04-11 07:21:38 +0700

    `Admin` - Api edit calendar p1

---
+++ app/Controllers/api/EditCalendarApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers\api;
+
+use App\Core\Controller\BaseController;
+use App\use_case\api\EditCalendarApiUseCase;
+
+class EditCalendarApiController extends BaseController
+{
+    public function edit() {
+        $id = $this->request->input->getInt('id');
+        $subject = $this->request->input->get('subject');
+        $full_name = $this->request->input->get('full_name');
+        $date = $this->request->input->get('date');
+        $time_start = $this->request->input->get('time_start');
+        $time_end = $this->request->input->get('time_end');
+        $doctor_id = $this->request->input->getInt('doctor_id');
+        $description = $this->request->input->get('description');
+        $use_case = new EditCalendarApiUseCase();
+    }
+}
