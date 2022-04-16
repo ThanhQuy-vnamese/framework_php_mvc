5dea1ed3c18556db863e93ebb193140e4cd14cd4
Trung Hieu <dangtrunghieu0207@gmail.com>
2022-04-16 15:37:43 -0700

    `Admin` - Active menu

--- public/js/src/pages/master/index.js
+++ public/js/src/pages/master/index.js
@@ -1,8 +1,12 @@
 import Logout from './Logout';
 import HistoryBack from './HistoryBack';
+import Menu from './Menu';
 
 const logout = new Logout();
 logout.render();
 
 const historyBack = new HistoryBack();
 historyBack.back();
+
+const menu = new Menu();
+menu.activeMenu();
