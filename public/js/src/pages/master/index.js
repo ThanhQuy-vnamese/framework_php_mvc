8c5c2339871a7acc5a1e3d897e79bbe4dab7f766
Trung Hieu <dangtrunghieu0207@gmail.com>
2022-04-16 10:23:49 -0700

    `Admin` - back previous page

--- public/js/src/pages/master/index.js
+++ public/js/src/pages/master/index.js
@@ -1,4 +1,8 @@
 import Logout from './Logout';
+import HistoryBack from './HistoryBack';
 
 const logout = new Logout();
 logout.render();
+
+const historyBack = new HistoryBack();
+historyBack.back();
