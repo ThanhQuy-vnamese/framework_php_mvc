389e3273f596aba81228e7c2e33524ab391fc6df
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-03 18:48:30 +0700

    `Admin` - Config i18next

--- public/js/src/common/translate/i18n.ts
+++ public/js/src/common/translate/i18n.ts
@@ -27,7 +27,7 @@ axios.get('/ajax/get-user-setting').then(result => {
     i18n.use(initReactI18next) // passes i18n down to react-i18next
         .init({
             resources,
-            lng: result.data.language, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+            lng: result.data.setting.language, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
             // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
             // if you're using a language detector, do not define the lng option
             fallbackLng: 'vi',
