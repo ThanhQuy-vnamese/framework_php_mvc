918191a9198d187e400993e2f43caf46334014e1
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-05 08:54:46 +0700

    `Admin` - Fix resource

---
+++ app/translates/resources/admin/user/eng.php
@@ -0,0 +1,20 @@
+<?php
+
+const LANGUAGES = [
+    // User List
+    'full_name' => 'Full name',
+    'role' => 'Role',
+    'email' => 'Email',
+    'phone' => 'Phone',
+    'birthday' => 'Birthday',
+    'status' => 'Status',
+    'action' => 'Action',
+        // User detail
+    'login_information' => 'Login Information',
+    'personal' => 'Personal',
+    'first_name' => 'First name',
+    'last_name' => 'Last name',
+    'email_address' => 'Email address',
+    'active' => 'Active',
+    'inactive' => 'Inactive'
+];
---
+++ app/translates/resources/admin/user/vi.php
@@ -0,0 +1,20 @@
+<?php
+
+const LANGUAGES = [
+    // User List
+    'full_name' => 'Họ và tên',
+    'role' => 'Quyền',
+    'email' => 'Email',
+    'phone' => 'Điện thoại',
+    'birthday' => 'Ngày sinh',
+    'status' => 'Trạng thái',
+    'action' => 'Hành động',
+    // User detail
+    'login_information' => 'Thông tin đăng nhập',
+    'personal' => 'Cá nhân',
+    'first_name' => 'Họ đệm',
+    'last_name' => 'Tên',
+    'email_address' => 'Địa chỉ email',
+    'active' => 'Đã kích hoạt',
+    'inactive' => 'Chưa kích hoạt'
+];
