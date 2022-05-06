bcd8cdc9ac67ff4a3922ab8ab608540f6a891f7f
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-06 22:04:08 +0700

    `Admin` - Done multi-language manage medicine type

--- app/translates/resources/admin/medicine/vi.php
+++ app/translates/resources/admin/medicine/vi.php
@@ -1,5 +1,12 @@
 <?php
 
 const LANGUAGES = [
-    'full_name' => 'Họ và tên'
+    'medicine_type_list' => 'Danh sách loại thuốc',
+    'add_medicine_type' => 'Thêm loại thuốc',
+    'medicine_type_information' => 'Thông tin loại thuốc',
+    'name' => 'Tên thuốc',
+    'description' => 'Mô tả',
+    'action' => 'Hành động',
+    'view_detail' => 'Xem chi tiết',
+    'delete_medicine_type' => 'Xóa loại thuốc',
 ];
