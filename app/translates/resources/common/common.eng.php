8e52e9179dbe2aa53b03ac666166a245e9305e07
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-06 22:27:47 +0700

    `Admin` - Fix bug

--- app/translates/resources/common/common.eng.php
+++ app/translates/resources/common/common.eng.php
@@ -4,6 +4,8 @@ const COMMON_LANGUAGES = [
     // Menu
     'manage_user' => 'Manage users',
     'manage_medical_files' => 'Manage medical files',
+    'manage_medicine' => 'Manage medicine',
+    'manage_medicine_type' => 'Manage medicine type',
     // Button
     'delete' => 'Delete',
     'cancel' => 'Cancel',
--- app/translates/resources/common/common.vi.php
+++ app/translates/resources/common/common.vi.php
@@ -4,6 +4,8 @@ const COMMON_LANGUAGES = [
     // Menu
     'manage_user' => 'Quản lí users',
     'manage_medical_files' => 'Quản lí hồ sơ bệnh nhân',
+    'manage_medicine' => 'Quản lí thuốc',
+    'manage_medicine_type' => 'Quản lí loại thuốc',
 
     // Button
     'delete' => 'Xóa',
