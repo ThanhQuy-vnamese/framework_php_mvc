605d65ab982c3acd0add72f0253e36200f7420e5
Anh Huynh <huynhkyanh18066031@gmail.com>
2022-05-07 22:13:14 +0700

    Fix menu footer

--- cache/d6/d6864c84aec10913e3d791a3aaa4b9f39bc178d85e627cbd3183ecd1c0728c54.php
+++ cache/d6/d6864c84aec10913e3d791a3aaa4b9f39bc178d85e627cbd3183ecd1c0728c54.php
@@ -64,21 +64,11 @@ class __TwigTemplate_e001a8aa895496e868ffb6337b45128a4bd8bba58a2d52056d4511d261f
 \t\t\t\t\t</div>
 \t\t\t\t</div>
 \t\t\t\t<div class=\"col-xl-6 col-sm-6 col-md-6 single-footer-widget\">
-\t\t\t\t\t<h4>Newsletter</h4>
-\t\t\t\t\t<p>Seed good winged wherein which night multiply midst does not fruitful</p>
-\t\t\t\t\t<div class=\"form-wrap\" id=\"mc_embed_signup\">
-\t\t\t\t\t\t<form target=\"_blank\" action=\"https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01\" method=\"get\" class=\"form-inline\">
-\t\t\t\t\t\t\t<input class=\"form-control\" name=\"EMAIL\" placeholder=\"Your Email Address\" onfocus=\"this.placeholder = ''\" onblur=\"this.placeholder = 'Your Email Address '\" required=\"\" type=\"email\">
-\t\t\t\t\t\t\t<button class=\"click-btn btn btn-default text-uppercase\">
-\t\t\t\t\t\t\t\t<i class=\"ti-angle-right\"></i>
-\t\t\t\t\t\t\t</button>
-\t\t\t\t\t\t\t<div style=\"position: absolute; left: -5000px;\">
-\t\t\t\t\t\t\t\t<input name=\"b_36c4fd991d266f23781ded980_aefe40901a\" tabindex=\"-1\" value=\"\" type=\"text\">
-\t\t\t\t\t\t\t</div>
-
-\t\t\t\t\t\t\t<div class=\"info\"></div>
-\t\t\t\t\t\t</form>
-\t\t\t\t\t</div>
+\t\t\t\t\t<h4>Contact</h4>
+\t\t\t\t\t<a href=\"";
+        // line 29
+        echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('redirect')->getCallable(), ["contact"]), "html", null, true);
+        echo "\">Contact with us</a>
 \t\t\t\t</div>
 \t\t\t</div>
 \t\t</div>
@@ -122,57 +112,57 @@ class __TwigTemplate_e001a8aa895496e868ffb6337b45128a4bd8bba58a2d52056d4511d261f
 
 <!-- jquery plugins here-->
 <script src=\"";
-        // line 85
+        // line 72
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/jquery-1.12.1.min.js"]), "html", null, true);
         echo "\"></script>
 
 <!-- popper js -->
 <script src=\"";
-        // line 88
+        // line 75
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/popper.min.js"]), "html", null, true);
         echo "\"></script>
 
 <!-- bootstrap js -->
 <script src=\"";
-        // line 91
+        // line 78
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/bootstrap.min.js"]), "html", null, true);
         echo "\"></script>
 
 <!-- owl carousel js -->
 <script src=\"";
-        // line 94
+        // line 81
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/owl.carousel.min.js"]), "html", null, true);
         echo "\"></script>
 <script src=\"";
-        // line 95
+        // line 82
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/jquery.nice-select.min.js"]), "html", null, true);
         echo "\"></script>
 
 <!-- contact js -->
 <script src=\"";
-        // line 98
+        // line 85
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/jquery.ajaxchimp.min.js"]), "html", null, true);
         echo "\"></script>
 <script src=\"";
-        // line 99
+        // line 86
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/jquery.form.js"]), "html", null, true);
         echo "\"></script>
 <script src=\"";
-        // line 100
+        // line 87
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/jquery.validate.min.js"]), "html", null, true);
         echo "\"></script>
 <script src=\"";
-        // line 101
+        // line 88
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/mail-script.js"]), "html", null, true);
         echo "\"></script>
 <script src=\"";
-        // line 102
+        // line 89
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/contact.js"]), "html", null, true);
         echo "\"></script>
 
 <!-- custom js -->
 <script src=\"";
-        // line 105
+        // line 92
         echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('createLink')->getCallable(), ["style/js/custom.js"]), "html", null, true);
         echo "\"></script>
 ";
@@ -190,7 +180,7 @@ class __TwigTemplate_e001a8aa895496e868ffb6337b45128a4bd8bba58a2d52056d4511d261f
 
     public function getDebugInfo()
     {
-        return array (  176 => 105,  170 => 102,  166 => 101,  162 => 100,  158 => 99,  154 => 98,  148 => 95,  144 => 94,  138 => 91,  132 => 88,  126 => 85,  46 => 8,  37 => 1,);
+        return array (  166 => 92,  160 => 89,  156 => 88,  152 => 87,  148 => 86,  144 => 85,  138 => 82,  134 => 81,  128 => 78,  122 => 75,  116 => 72,  70 => 29,  46 => 8,  37 => 1,);
     }
 
     public function getSourceContext()
@@ -222,21 +212,8 @@ class __TwigTemplate_e001a8aa895496e868ffb6337b45128a4bd8bba58a2d52056d4511d261f
 \t\t\t\t\t</div>
 \t\t\t\t</div>
 \t\t\t\t<div class=\"col-xl-6 col-sm-6 col-md-6 single-footer-widget\">
-\t\t\t\t\t<h4>Newsletter</h4>
-\t\t\t\t\t<p>Seed good winged wherein which night multiply midst does not fruitful</p>
-\t\t\t\t\t<div class=\"form-wrap\" id=\"mc_embed_signup\">
-\t\t\t\t\t\t<form target=\"_blank\" action=\"https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01\" method=\"get\" class=\"form-inline\">
-\t\t\t\t\t\t\t<input class=\"form-control\" name=\"EMAIL\" placeholder=\"Your Email Address\" onfocus=\"this.placeholder = ''\" onblur=\"this.placeholder = 'Your Email Address '\" required=\"\" type=\"email\">
-\t\t\t\t\t\t\t<button class=\"click-btn btn btn-default text-uppercase\">
-\t\t\t\t\t\t\t\t<i class=\"ti-angle-right\"></i>
-\t\t\t\t\t\t\t</button>
-\t\t\t\t\t\t\t<div style=\"position: absolute; left: -5000px;\">
-\t\t\t\t\t\t\t\t<input name=\"b_36c4fd991d266f23781ded980_aefe40901a\" tabindex=\"-1\" value=\"\" type=\"text\">
-\t\t\t\t\t\t\t</div>
-
-\t\t\t\t\t\t\t<div class=\"info\"></div>
-\t\t\t\t\t\t</form>
-\t\t\t\t\t</div>
+\t\t\t\t\t<h4>Contact</h4>
+\t\t\t\t\t<a href=\"{{ redirect('contact') }}\">Contact with us</a>
 \t\t\t\t</div>
 \t\t\t</div>
 \t\t</div>
