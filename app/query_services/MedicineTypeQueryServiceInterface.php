e626f7d0e61f746cd1d4551856a6bc17c9ab47f2
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-14 12:37:38 +0700

    `Admin` - Fix bug

--- app/query_services/MedicineTypeQueryServiceInterface.php
+++ app/query_services/MedicineTypeQueryServiceInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\query_services;
 
 use App\dto\MedicineType;
+use App\dto\MedicineTypeListDto;
 
 interface MedicineTypeQueryServiceInterface
 {
@@ -13,4 +14,10 @@ interface MedicineTypeQueryServiceInterface
     public function getTotalMedicineType(): int;
 
     public function getMedicineTypeById(int $id): MedicineType;
+
+    /**
+     * @return MedicineTypeListDto[]
+     */
+    public function getAllMedicineTypeNotByOffset(): array;
+
 }
