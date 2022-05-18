d40a7ea12bb335656e4360d3a38720302b5773b9
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-18 23:12:25 +0700

    `User` - Multi language

--- app/translates/resources/common/common.vi.php
+++ app/translates/resources/common/common.vi.php
@@ -15,5 +15,9 @@ const COMMON_LANGUAGES = [
     'delete' => 'Xóa',
     'cancel' => 'Hủy',
     'back' => 'Trở về',
-    'log_out' => 'Đăng xuất'
+    'log_out' => 'Đăng xuất',
+    'login' => 'Đăng nhập',
+    'profile' => 'Thông tin cá nhân',
+    'search_doctor' => 'Tìm bác sĩ',
+    'search' => 'Tìm kím',
 ];
